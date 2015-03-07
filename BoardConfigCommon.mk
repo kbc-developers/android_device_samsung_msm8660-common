8650647b4c0b94496bc7f988f0ac572013346e98
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-07 00:14:03 -0500

    selinux: sort of a revert adding back what was removed until we get to the bottom of 3 button MTP menu in storage
    Change-Id: I15a6dbfac37cf0a1c0b0121f4b297198af6273c7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,11 +104,16 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung/msm8660-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    app.te \
     bluetooth.te \
     bootanim.te \
     device.te \
+    domain.te \
+    drmserver.te \
     file.te \
     file_contexts \
+    healthd.te \
+    init.te \
     gamma_dev.te \
     genfs_contexts \
     init_shell.te \
@@ -137,6 +142,7 @@ BOARD_SEPOLICY_UNION += \
     ueventd.te \
     untrusted_app.te \
     vold.te \
+    wpa_socket.te \
     wpa.te
 
 # Wifi related defines
