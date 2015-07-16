ea99c82672d23bbc34299ea4f66924b347ed1c49
Nick Reuter <nreuter85@gmail.com>
2015-07-16 02:51:51 -0500

    Revert "jf-common: disable LOGD"
    This reverts commit 0bbc7657b269e383279ae1541df6131aa43ba391.
    
    Change-Id: Ib10e2d8011229beef2c9575f2203389dda3fefaa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,8 +67,9 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/msm8660-common/include
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Logging
-TARGET_USES_LOGD := false
+# Media
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+TARGET_QCOM_MEDIA_VARIANT := caf
 
 # Power
 TARGET_POWERHAL_VARIANT := cm
