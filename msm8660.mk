773b41dc0328c1b0d3a15afa61133aa476f2a134
bryan2894 <bryan2894@gmail.com>
2017-02-07 02:00:28 -0500

    msm8660: set QAN elements to 5
    * Needed for network search
    
    Change-Id: Iff150cb5855289a81b7106e8450e56deb6143bc1

--- msm8660.mk
+++ msm8660.mk
@@ -70,7 +70,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL Class
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungMSM8660RIL
+    ro.telephony.ril_class=SamsungMSM8660RIL \
+    ro.ril.telephony.mqanelements=5
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
